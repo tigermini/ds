6311bf40aae7c1548724ff6c081bbf5e779e5a2f
tigermini <tigermini@126.com>
2021-08-27 23:01:58 +0800

    规范二进制查找代码格式

--- binary_search/main.c
+++ binary_search/main.c
@@ -1,19 +1,20 @@
-int search(int* nums, int numsSize, int target){
-    int left, right, index;
+int search(int *nums, int numsSize, int target)
+{
+	int left, right, index;
 
-    left = 0;
-    right = numsSize - 1;
+	left = 0;
+	right = numsSize - 1;
 
-    while (left <= right) {
-        index = (right + left) / 2;
-        if (target == nums[index]) {
-            return index;
-        } else if (target < nums[index]) {
-            right = index - 1;
-        } else {
-            left = index + 1;
-        }
-    }
+	while (left <= right) {
+		index = (right + left) / 2;
+		if (target == nums[index]) {
+			return index;
+		} else if (target < nums[index]) {
+			right = index - 1;
+		} else {
+			left = index + 1;
+		}
+	}
 
-    return -1;
+	return -1;
 }
